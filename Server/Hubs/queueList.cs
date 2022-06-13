dda78552c0afc4b1a033630921ef8aa28c56f311
baylenjones <baylen.jones0224@gmail.com>
2022-06-13 17:23:50 -0500

    20220613

---
+++ Server/Hubs/queueList.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Procuts.Server.Hubs
+{
+	public class queueList : Hub
+	{
+
+		public override async Task OnConnectedAsync()
+        {
+			await SendMessage("", "User Connected!");
+			await base.OnConnectedAsync();
+        }
+
+		public async Task SendMessage(string user, string message)
+        {
+			await Clients.All.SendAsync("Update", user, message);
+        }
+	}
+}
+
